9b19de13e4faec6557dd7942cd9167fe3f5b0412
indura08 <induraperera3@gmail.com>
2024-03-15 01:25:59 +0530

    commit to update chnages in index.js and package json files

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
 
 //methna process.env kiya kiya denne menna meken hdgnna eka thami. process.env thiynne smahara hangnna one dewal project ekn hanganna 
 dotenv.config();
@@ -17,5 +20,8 @@ app.use(express.json())
 
 app.use("/api/auth" , authRoute);
 app.use("/api/users" , userRoute);
+app.use("/api/products" , productRoute);
+app.use("/api/cart" , cartRoute)
+app.use("/api/orders" , orderRoute);
 
 app.listen(process.env.PORT || 5002, () => console.log("app is listen on port 5002"));
